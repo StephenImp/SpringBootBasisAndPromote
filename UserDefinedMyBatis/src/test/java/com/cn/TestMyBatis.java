3c91f2de5b95d4ec2277a3e5aad5923d7d051990
StephenImp <390168303@qq.com>
2018-10-05 10:11:48 +0800

    整合后第一版

---
+++ UserDefinedMyBatis/src/test/java/com/cn/TestMyBatis.java
@@ -0,0 +1,68 @@
+package com.cn;
+
+import com.cn.entity.TUser;
+import com.cn.mapper.TUserMapper;
+import com.cn.session.SqlSession;
+import com.cn.session.SqlSessionFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+
+/**
+ * Created by Administrator on 2018\9\12 0012.
+ */
+public class TestMyBatis {
+
+    //1.实例化sqlSessionFactory,加载数据库配置文件信息以及mapper.xml文件到configuration对象
+    SqlSessionFactory factory = new SqlSessionFactory();
+
+    @Test
+    public void testMyBatis(){
+
+        //2.获取sqlsession对象
+        SqlSession session = factory.opeanSession();
+
+        //3.通过代理模式跨越面向接口编程和ibatis编程模型的鸿沟
+        TUserMapper userMapper = session.getMapper(TUserMapper.class);
+
+        //4.遵循jdbc规范，通过底层的4大对象的合作完成数据查询和数据转化
+        TUser user = userMapper.selectByPrimaryKey(1);
+
+        System.out.println(user);
+
+        System.out.println("-------------------------------");
+
+        List<TUser> selectAll = userMapper.selectAll();
+
+        for (TUser tUser : selectAll) {
+            System.out.println(tUser);
+        }
+
+    }
+
+    /**
+     * 本质分析，基于ibatis编程模型
+     * @throws IOException
+     */
+    @Test
+    public void originalOperation()throws IOException{
+
+        //2.获取sqlSession
+        SqlSession sqlSession = factory.opeanSession();
+
+        //3.执行查询语句并返回结果
+        TUser user = sqlSession.selectOne("",1);
+        System.out.println(user.toString());
+
+        System.out.println("----------------------------------------------");
+
+        List<TUser> users = sqlSession.selectList("",0);
+
+        for (TUser tUser:users) {
+            System.out.println(tUser);
+        }
+    }
+}
